a9c738470461af89d9b082a036d517f40466b1eb
Marco Crespo <marcoc8@Marcos-MacBook-Air.local>
2022-11-02 16:36:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,127 @@
+// I wanted to work on orginazing my code and making it easier to read 
+
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const score = document.querySelector('#score');
+const resetButton = document.querySelector('#resetButton');
+const boardWidth = gameBoard.width;
+const boardHeight = gameBoard.height;
+const paddleBorder = 'black';
+const paddle1Color = 'pink';
+const paddle2Color = 'yellow';
+const ballColor = 'black';
+let ballX = boardWidth / 2;
+let ballY = boardHeight / 2;
+let ballSpeed;
+let ballXTragectory = 0;
+let ballYTragectory = 0;
+let intervalId;
+let player1Score = 0;
+let player2Score = 0;
+
+// Paddles for player 1 and 2. 
+
+const paddle1 = {
+    width: 25,
+    height: 75,
+    x: 0,
+    y: 0
+};
+const paddle2 = {
+    width: 25,
+    height: 75,
+    x: boardWidth - 25,
+    y: boardHeight - 75
+};
+
+paddles();
+
+function paddles () {
+    ctx.strokeStyle = paddleBorder;
+
+    ctx.fillStyle = paddle1Color;
+    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+
+    ctx.fillStyle = paddle2Color;
+    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+    ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+
+};
+       
+function paddleMovement(){
+    window.addEventListener('keydown', function(event){
+
+        if(event.code === 'KeyW'){
+            paddle1.y -= 20;
+            clearBoard();
+          }
+        
+          if(event.code === 'KeyS'){
+            paddle1.y += 20;
+            clearBoard();
+          }
+        
+          if(event.code === 'ArrowUp'){
+            paddle2.y -= 20;
+            clearBoard();
+          }
+        
+          if(event.code === 'ArrowDown'){
+            paddle2.y += 20;
+            clearBoard();
+          }
+      
+      })
+};
+
+paddleMovement();
+
+// Ping Pong Ball
+
+function ball (ballX, ballY){
+    ctx.fillStyle = ballColor;
+    ctx.lineWidth = 2;
+    ctx.arc(ballX, ballY, 12.5, 0, 2 * Math.PI)
+    ctx.stroke();
+    ctx.fill();
+    };
+    
+    ball();
+
+function newBall (){
+        ballSpeed = 1;
+        if(Math.round(Math.random()) == 1){
+            ballXTragectory =  1; 
+        }
+        else{
+            ballXTragectory = -1; 
+        }
+        if(Math.round(Math.random()) == 1){
+            ballYTragectory = Math.random() * 1; //more random directions
+        }
+        else{
+            ballYTragectory = Math.random() * -1; //more random directions
+        }
+        ballX = gameWidth / 2;
+        ballY = gameHeight / 2;
+        ball(ballX, ballY);
+    };
+
+    newBall();
+    
+    function moveBall(){
+        ballX += (ballSpeed * ballXTragectory);
+        ballY += (ballSpeed * ballYTragectory);
+        ball(ballX, ballY)
+    };
+
+    moveBall();
+
+
+function clearBoard (){
+    ctx.clearRect(0, 0, boardWidth, boardHeight)
+    paddles();
+    ball(ballX, ballY);
+    moveBall();
+};
